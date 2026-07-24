c76a84332266ea09e2cb77bfeba14fa28105cf2b
Edward Mirzoyants <Edward532398@gmail.com>
2026-07-25 00:31:55 +0300

    Добавлена регистрация, личный кабинет, бронирование, PostgreSQL, Docker

--- users/views.py
+++ users/views.py
@@ -1,7 +1,7 @@
 from django.shortcuts import render, redirect
 from django.contrib import messages
 from django.contrib.auth.models import User
-from django.contrib.auth import login
+from django.contrib.auth import login, authenticate
 from django.contrib.auth.decorators import login_required
 from bookings.models import Booking
 
